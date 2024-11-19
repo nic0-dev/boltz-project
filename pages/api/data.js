a65191ac3d75ebcc4bd2bf775c158716b0c207be
nic0-dev <mlcagas@up.edu.ph>
2024-11-19 19:33:41 +0800

    Trigger redeployment

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,10 +1,7 @@
 export default async function handler(req, res) {
     try {
-        // Access the public folder directly
         const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}/data.json`);
         const data = await response.json();
-
-        // Send the data in the API response
         res.status(200).json({ data });
     } catch (error) {
         console.error("Error fetching data from /public/data.json:", error);
